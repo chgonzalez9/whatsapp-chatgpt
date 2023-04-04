2d4ddf21c7d86fbf1f5fed24df26d1605045b5f9
Christian <thech999@gmail.com>
2023-04-04 23:58:45 +0200

    dalle test

--- src/handlers/dalle.ts
+++ src/handlers/dalle.ts
@@ -46,4 +46,43 @@ const handleMessageDALLE = async (message: any, prompt: any) => {
 	}
 };
 
-export { handleMessageDALLE };
+const handleEditImageDALLE = async (message: any, prompt: any) => {
+	try {
+		const start = Date.now();
+
+		cli.print(`[DALL-E] Received prompt from ${message.from}: ${prompt}`);
+
+		// Prompt Moderation
+		if (config.promptModerationEnabled) {
+			try {
+				await moderateIncomingPrompt(prompt);
+			} catch (error: any) {
+				message.reply(error.message);
+				return;
+			}
+		}
+
+		// Send the prompt to the API
+		const response = await openai.createImageEdit({
+			image: image,
+			prompt: prompt,
+			n: 1,
+			size: aiConfig.dalle.size as CreateImageRequestSizeEnum,
+			response_format: "b64_json"
+		});
+
+		const end = Date.now() - start;
+
+		const base64 = response.data.data[0].b64_json as string;
+		const image = new MessageMedia("image/jpeg", base64, "image.jpg");
+
+		cli.print(`[DALL-E] Answer to ${message.from} | OpenAI request took ${end}ms`);
+
+		message.reply(image);
+	} catch (error: any) {
+		console.error("An error occured", error);
+		message.reply("An error occured, please contact the administrator. (" + error.message + ")");
+	}
+};
+
+export { handleMessageDALLE, handleEditImageDALLE };
